9aff0e66cf7132702fd7054808712738d6fa13c1
SazidAhmed <sazidahmed.official@gmail.com>
2022-05-27 23:45:14 +0600

    fixing validation bugs

--- server/app/Blockchain/Validation.js
+++ server/app/Blockchain/Validation.js
@@ -1,16 +1,14 @@
 
-function isChainValid(chain) {
+function isChainValid(myChain) {
+    return myChain
     // Check the remaining blocks on the chain to see if there hashes
     for (let i = 1; i < chain.length; i++) {
         const currentBlock = chain[i];
         const previousBlock = chain[i - 1];
 
-        if (previousBlock.hash !== currentBlock.previousHash) {
-        return false;
-        }
-
-        if (currentBlock.hash !== currentBlock.calculateHash()) {
-        return false;
+        //hash of current block should be same after recalculating the hash of current block
+        if (currentBlock.hash != currentBlock.calculateHash()){
+            return false;
         }
     }
 
--- server/app/Blockchain/main.js
+++ server/app/Blockchain/main.js
@@ -6,17 +6,15 @@ const { isChainValid } = require('./Validation');
 // const block = new Block(Date.parse('2017-01-01'), [], '0');
 
 let myChain = new Blockchain();
-myChain.addBlock(new Block(Date.now(), {amount: 4}))
-myChain.addBlock(new Block(Date.now(), {amount: 20}))
 myChain.addBlock(new Block(Date.now(), {amount: 40}))
+myChain.addBlock(new Block(Date.now(), {amount: 20}))
 
-console.log(JSON.stringify(myChain, null, 4))
+chainJson = JSON.stringify(myChain, null, 4)
+console.log(chainJson[0])
 
-chainValid = isChainValid(myChain)
+// console.log('is chain valid ? ' + isChainValid(myChain))
 
-console.log('is chain valid ? ' + chainValid)
+// myChain.chain[1].transactions = { amount : 100 }
+// myChain.chain[1].hash = myChain.chain[1].calculateHash()
 
-myChain.chain[1].transactions = {amount : 100}
-
-chainValid = isChainValid(myChain)
-console.log('is chain valid after mutation ? ' + chainValid)
+// console.log('is chain valid after mutation ? ' + isChainValid(myChain))
