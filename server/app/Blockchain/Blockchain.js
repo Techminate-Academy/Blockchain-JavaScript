36c841cb0665be838afc167afa09fdb7ef9e7080
SazidAhmed <sazidahmed.official@gmail.com>
2022-05-28 22:21:45 +0600

    validation test completed

--- server/app/Blockchain/Blockchain.js
+++ server/app/Blockchain/Blockchain.js
@@ -36,6 +36,28 @@ class Blockchain {
     newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
+
+  isChainValid() {
+    // Check if the Genesis block hasn't been tampered with by comparing
+    // the output of createGenesisBlock with the first block on our chain
+
+    // Check the remaining blocks on the chain to see if there hashes and
+    // signatures are correct
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (previousBlock.hash !== currentBlock.previousHash) {
+        return false;
+      }
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+    }
+
+    return true;
+  }
   
 }
 
--- server/app/Blockchain/main.js
+++ server/app/Blockchain/main.js
@@ -1,7 +1,6 @@
 // const Block = require('./Block');
 const Block = require('./Block');
 const Blockchain = require('./Blockchain');
-const { isChainValid } = require('./Validation');
 
 // const block = new Block(Date.parse('2017-01-01'), [], '0');
 
@@ -9,12 +8,12 @@ let myChain = new Blockchain();
 myChain.addBlock(new Block(Date.now(), {amount: 40}))
 myChain.addBlock(new Block(Date.now(), {amount: 20}))
 
-chainJson = JSON.stringify(myChain, null, 4)
-console.log(chainJson[0])
+var chainJson = JSON.stringify(myChain, null, 4)
+console.log(chainJson)
 
-// console.log('is chain valid ? ' + isChainValid(myChain))
+console.log('is chain valid ? ' + myChain.isChainValid())
 
-// myChain.chain[1].transactions = { amount : 100 }
-// myChain.chain[1].hash = myChain.chain[1].calculateHash()
+myChain.chain[1].transactions = { amount : 100 }
+myChain.chain[1].hash = myChain.chain[1].calculateHash()
 
-// console.log('is chain valid after mutation ? ' + isChainValid(myChain))
+console.log('is chain valid after mutation ? ' + myChain.isChainValid())
