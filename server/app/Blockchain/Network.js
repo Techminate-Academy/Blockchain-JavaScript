a5817ca58be9e90432c988475fca18cef20080e8
SazidAhmed <sazidahmed.official@gmail.com>
2022-06-09 21:53:14 +0600

    connect nodes completed

--- server/app/Blockchain/Network.js
+++ server/app/Blockchain/Network.js
@@ -1,9 +1,17 @@
 function ConnectNodes(chain, req) {
     const { nodes } = req.body
+
     if (nodes.length > 0){
-        for (let i = 1; i < nodes.length; i++) {
-            console.log(nodes[i])
-          }
+        for (let i = 0; i < nodes.length; i++) {
+            chain.addNodes(nodes[i])
+        }
+
+        let data = {
+            'message' : 'Connected to the network',
+            'node_list' : chain.nodes
+        }
+
+        return data;
     }else{
         console.log('no nodes to add')
     }
